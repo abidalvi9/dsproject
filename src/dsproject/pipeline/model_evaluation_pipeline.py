57aeab89e5ba92f1cf2f38c20af812cf059a385a
abidalvi9 <abhidalvi24@gmail.com>
2025-03-20 15:19:37 +0530

    model evaluation updated

---
+++ src/dsproject/pipeline/model_evaluation_pipeline.py
@@ -0,0 +1,15 @@
+from src.dsproject.config.configuration import ConfigurationManager
+from src.dsproject.components.model_evaluation import ModelEvaluation
+from src.dsproject import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.log_into_mlflow()
